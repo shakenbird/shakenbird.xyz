d74e5dc103bdc5482414ee519151793cf36f79fb
shaken_bird <77373750+shakenbird@users.noreply.github.com>
2022-04-19 22:30:47 -0700

    add js for page updates

--- articles/a/igp-guidelines/script.js
+++ articles/a/igp-guidelines/script.js
@@ -4,3 +4,10 @@ function copyFunction() {
 function printFunction() {
   window.print()
 }
+function formatDaysAgo(value, locale) {
+  const date = new Date(value);
+  const deltaDays = (date.getTime() - Date.now()) / (1000 * 3600 * 24);
+  const formatter = new Intl.RelativeTimeFormat(locale);
+  return formatter.format(Math.round(deltaDays), 'days');
+}
+document.getElementById("post").innerHTML = "Updated " + formatDaysAgo(new Date('2022-04-18'))
