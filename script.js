eb78c5c61285f5b78db7e179be81e542eaeb36ab
shaken_bird <77373750+shakenbird@users.noreply.github.com>
2022-05-10 21:55:50 -0700

    added support for theme toggler

--- script.js
+++ script.js
@@ -1,43 +1,13 @@
-function Time() {
- var date = new Date();
- var hour = date.getHours();
- var minute = date.getMinutes();
- var second = date.getSeconds();
- // Variable to store AM / PM
- var period = "";
- // Assigning AM / PM according to the current hour
- if (hour >= 12) {
- period = "PM";
- } else {
- period = "AM";
- }
- // Converting the hour in 12-hour format
- if (hour == 0) {
- hour = 12;
- } else {
- if (hour > 12) {
- hour = hour - 12;
- }
- }
- // Updating hour, minute, and second
- // if they are less than 10
- hour = update(hour);
- minute = update(minute);
- second = update(second);
- // Adding time elements to the div
- document.getElementById("digital-clock").innerText = hour + ":" + minute + ":" + second + " " + period;
- // Set Timer to 1 sec (1000 ms)
- setTimeout(Time, 1000);
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  document.body.classList.toggle("dark")
+  document.getElementById("moon").classList.toggle("light-icon")
+  document.getElementById("sun").classList.toggle("dark-icon")
+  document.getElementById("main").classList.toggle("main")
 }
- // Function to update time elements if they are less than 10
- // Append 0 before time elements if they are less than 10
-function update(t) {
- if (t < 10) {
- return "0" + t;
- }
- else {
- return t;
- }
+function themeToggler() {
+  document.body.classList.toggle("dark")
+  document.getElementById("moon").classList.toggle("light-icon")
+  document.getElementById("sun").classList.toggle("dark-icon")
+  document.getElementById("main").classList.toggle("main")
+  document.getElementsByClassName("name").classList.toggle("dark-name")
 }
-Time();
-"use strict";
